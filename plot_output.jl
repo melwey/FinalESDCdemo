198b7d579f56b21bd0fd2952c6d53232e60e07d0
Melanie Weynants <45990429+melwey@users.noreply.github.com>
2024-02-14 10:54:57 +0100

    fix units

--- plot_output.jl
+++ plot_output.jl
@@ -10,7 +10,7 @@ input = open_dataset("/Net/Groups/BGI/work_3/scratch/fgans/DeepCube/UC3Cube_rech
 ds = open_dataset("/Net/Groups/BGI/people/fgans/DeepCube/FinalESDCDEmo/output.zarr/")
 
 preds = ("lst_night", "lst_day","dem", "lc_forest", "lc_grassland", "roads_distance")
-units = ("Temperature [Kelvin]","Temperature [Kelvin]","Elevation [m]", "Forest cover [%]", "Grass cover [%]", "Distance to nearest road [km]")
+units = ("Difference in temperature [Kelvin]","Difference in temperature [Kelvin]","Difference in elevation [m]", "Difference in forest cover [%]", "Difference in grass cover [%]", "Difference in distance to nearest road [km]")
 possible_predictors = map(i->ds[Symbol(i)],preds);
 
 if !isdir("./figs")
